87fa39e5fddc1316cfe4ab41cc0d17c63c326c6c
shubham6m <shubhammishra6m@gmail.com>
2025-02-14 23:52:27 +0530

    add .env to gitignore

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { google } = require('googleapis');
 const app = express();
-const port = process.env.GOOGLE_APPLICATION_CREDENTIALS || 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 // const serviceAccountKey = require("./public/attend.json")
 let fetch;
